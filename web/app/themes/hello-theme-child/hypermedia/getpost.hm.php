4b8b9c82969688b60158bbb10b87061a3bc19790
Michael Wender <michael@michaelwender.com>
2025-09-18 14:42:34 -0400

    Adding debug output

--- web/app/themes/hello-theme-child/hypermedia/getpost.hm.php
+++ web/app/themes/hello-theme-child/hypermedia/getpost.hm.php
@@ -1,7 +1,10 @@
 <?php
 // Basic nonce validation (works for all hypermedia libraries)
 if ( ! hm_validate_request() ) {
-  hm_die( 'Security check failed' );
+  uber_log('🔔 $_REQUEST = ', $_REQUEST );
+  uber_log('👉 $_SERVER[\'HTTP_X_WP_NONCE\'] = ', $_SERVER['HTTP_X_WP_NONCE'] );
+
+  hm_die( 'Security check failed. $_REQUEST = ' . print_r( $_REQUEST, true ) . '; HTTP_X_WP_NONCE = ' . $_SERVER['HTTP_X_WP_NONCE'] );
 }
 
 if( 'development' == WP_ENVIRONMENT_TYPE )
